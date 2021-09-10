eefbca8f8a790f9648c7a37deb5ca51164624a8d
wesselrijk <wesselrijk@gmail.com>
2021-09-10 14:00:00 +0200

    put it all up

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Created by Wessel Rijk
+
+This python program is going to use .root files of ALICE jet data (i.e. Pythia, but maybe real data?). Any data should be imported and turned into pandas sets. Afterwards Machine Learning algorithms will be used to do anomaly search with data.
+"""
+
+import numpy as np
